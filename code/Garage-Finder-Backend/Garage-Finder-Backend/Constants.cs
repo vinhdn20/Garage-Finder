21e6a651bd8ac62a9b8d5053a681330e53cc7339
vinhdn20 <vinhdn20@gmail.com>
2023-05-24 20:46:07 +0700

    change login-gg api

---
+++ code/Garage-Finder-Backend/Garage-Finder-Backend/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_Finder_Backend
+{
+    public class Constants
+    {
+        public const int ROLE_GARAGE = 0;
+        public const int ROLE_CAR = 1;
+    }
+}
