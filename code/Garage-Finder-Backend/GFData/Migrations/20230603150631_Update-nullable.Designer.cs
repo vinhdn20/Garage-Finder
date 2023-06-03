ca87e81e9d2a9b2c6290f0645d218e871f24d3de
vinhdn20 <vinhdn20@gmail.com>
2023-06-03 22:09:29 +0700

    update migration

--- code/Garage-Finder-Backend/GFData/Migrations/20230603150631_Update-nullable.Designer.cs
+++ code/Garage-Finder-Backend/GFData/Migrations/20230603150631_Update-nullable.Designer.cs
@@ -4,6 +4,7 @@ using GFData.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GFData.Migrations
 {
     [DbContext(typeof(GFDbContext))]
-    partial class GFDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230603150631_Update-nullable")]
+    partial class Updatenullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
