20da993eb767d200e134dbe47768b29acd9d5e72
vinhdn20 <vinhdn20@gmail.com>
2023-06-03 21:51:23 +0700

    update database

--- code/Garage-Finder-Backend/GFData/Models/Entity/Garage.cs
+++ code/Garage-Finder-Backend/GFData/Models/Entity/Garage.cs
@@ -9,17 +9,20 @@ namespace GFData.Models.Entity
         public int GarageID { get; set; }
         [ForeignKey("User")]
         public int UserID { get; set; }
+        [Required]
         public string GarageName { get; set; }
         public string Address { get; set; }
+        [Required]
         public string EmailAddress { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
-        public string Status { get; set; }
-        public double OpenTime { get; set; }
-        public double CloseTime { get; set; }
-        public string Logo { get; set; }
+        public string? Status { get; set; }
+        public double? OpenTime { get; set; }
+        public double? CloseTime { get; set; }
+        public string? Logo { get; set; }
         public string? Imagies { get; set; }
-        public double LatAddress { get; set; }
-        public double LngAddress { get; set; }
+        public double? LatAddress { get; set; }
+        public double? LngAddress { get; set; }
         public ICollection<Orders> Orders { get; set; }
         public ICollection<Service> Services { get; set; }
         public ICollection<FavoriteList> FavoriteList { get; set; }
