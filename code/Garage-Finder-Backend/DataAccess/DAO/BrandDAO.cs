e69eec021e191d818eea996f9e261dfbee6a5a88
GIOSS\Gios <nguyenphongqh183@gmail.com>
2023-06-03 21:30:01 +0700

    add brand controller

---
+++ code/Garage-Finder-Backend/DataAccess/DAO/BrandDAO.cs
@@ -0,0 +1,99 @@
+﻿using GFData.Data;
+using GFData.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+    public class BrandDAO
+    {
+        private static BrandDAO instance = null;
+        private static readonly object iLock = new object();
+        public BrandDAO()
+        {
+
+        }
+
+        public static BrandDAO Instance
+        {
+            get
+            {
+                lock (iLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new BrandDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public List<Brand> GetBrand()
+        {
+            var listBrands = new List<Brand>();
+            try
+            {
+                using (var context = new GFDbContext())
+                {
+                    listBrands = context.Brand.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return listBrands;
+        }
+
+        public void Add(Brand brand)
+        {
+            try
+            {
+                var db = new GFDbContext();
+                db.Brand.Add(brand);
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public void Update(Brand c)
+        {
+            try
+            {
+                using (var context = new GFDbContext())
+                {
+                    context.Entry<Brand>(c).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                using (var context = new GFDbContext())
+                {
+                    var cDelete = context.Brand.SingleOrDefault(x => x.BrandID == id);
+                    context.Brand.Remove(cDelete);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ code/Garage-Finder-Backend/Garage-Finder-Backend/Controllers/brandController.cs
@@ -0,0 +1,69 @@
+﻿using DataAccess.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.Interfaces;
+
+namespace Garage_Finder_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly IBrandRepository brandRepository;
+
+        public BrandController(IBrandRepository brandRepository)
+        {
+            this.brandRepository = brandRepository;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(brandRepository.GetBrand());
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add(BrandDTO brand)
+        {
+            try
+            {
+                brandRepository.Add(brand);
+
+                return Ok("Success");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Update(BrandDTO brand)
+        {
+            try
+            {
+                brandRepository.Update(brand);
+
+                return Ok("Success");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                brandRepository.Delete(id);
+                return Ok("SUCCESS");
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
