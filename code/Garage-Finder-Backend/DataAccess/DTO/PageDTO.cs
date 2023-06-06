a42a25420406cc0c7f07d18f533b7ef40a2ffc6c
GIOSS\Gios <nguyenphongqh183@gmail.com>
2023-06-06 21:10:07 +0700

    Phong add code PageIndex and getGarageByUser

---
+++ code/Garage-Finder-Backend/DataAccess/DTO/PageDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DTO
+{
+    public class PageDTO
+    {
+        public int pageNumber { get; set; }
+        public int pageSize { get; set; }
+    }
+}
