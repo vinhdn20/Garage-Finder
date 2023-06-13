442009ee59e5dd937dbb81e229d50afa49719f1d
GIOSS\Gios <nguyenphongqh183@gmail.com>
2023-06-13 23:27:08 +0700

    update code

--- code/Garage-Finder-Backend/Repositories/Interfaces/IGarageRepository.cs
+++ code/Garage-Finder-Backend/Repositories/Interfaces/IGarageRepository.cs
@@ -14,7 +14,7 @@ namespace Repositories.Interfaces
         void Update(UpdateGarageDTO garage);
         List<GarageDTO> GetGarages();
         void DeleteGarage(int id);
-        public List<GarageDTO> GetGaragesByID(int? id);
+        public List<GarageDTO> GetGaragesByID(int id);
         public List<GarageDTO> GetByPage(PageDTO p);
         public List<GarageDTO> GetGarageByUser(int? id);
         public List<GarageDTO> GetGarageByProviceId(int? provinceID);
