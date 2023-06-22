cfc64f730458f14e5f059786ff0ef5eade518bef
vinhdn20 <vinhdn20@gmail.com>
2023-06-22 23:28:24 +0700

    update brand dto

--- code/Garage-Finder-Backend/DataAccess/DTO/BrandDTO.cs
+++ code/Garage-Finder-Backend/DataAccess/DTO/BrandDTO.cs
@@ -11,5 +11,6 @@ namespace DataAccess.DTO
         public int BrandID { get; set; }
         public string BrandName { get; set; }
         public string Note { get; set; }
+        public string? ImageLink { get; set; }
     }
 }
