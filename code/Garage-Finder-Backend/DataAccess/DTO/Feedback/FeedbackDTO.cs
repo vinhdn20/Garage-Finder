5581e89891133a1da1167f3e97d3d57b1ef2a0be
vinhdn20 <vinhdn20@gmail.com>
2023-07-16 15:34:15 +0700

    update feedback return

--- code/Garage-Finder-Backend/DataAccess/DTO/Feedback/FeedbackDTO.cs
+++ code/Garage-Finder-Backend/DataAccess/DTO/Feedback/FeedbackDTO.cs
@@ -5,6 +5,7 @@ namespace DataAccess.DTO.Feedback
     public class FeedbackDTO
     {
         public int FeedbackID { get; set; }
+        public string Name { get; set; }
         public int Star { get; set; }
         public string Content { get; set; }
         public DateTime DateTime { get; set; }
