01ceb0f82007a56c5ef0d84436f75e4c03be6d58
GIOSS\Gios <nguyenphongqh183@gmail.com>
2023-08-10 00:23:53 +0700

    add code

--- code/Garage-Finder-Backend/Garage-Finder-Backend/Controllers/GarageController.cs
+++ code/Garage-Finder-Backend/Garage-Finder-Backend/Controllers/GarageController.cs
@@ -151,7 +151,7 @@ namespace Garage_Finder_Backend.Controllers
 
             if (!string.IsNullOrEmpty(searchGarage.keyword))
             {
-                garages = garages.Where(g => g.GarageName.ToLower().Contains(searchGarage.keyword.ToLower())).ToList();
+                garages = garages.Where(g => g.GarageName.convertToUnSign3().ToLower().Contains(searchGarage.keyword.ToLower())).ToList();
             }
 
             if (searchGarage.provinceID is not null)
